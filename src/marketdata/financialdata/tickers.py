e80bad3f2a80f0346ae2ba5322d2421d11fbb61f
CMWolfe <cmwolfe1123@gmail.com>
2022-11-05 09:56:05 -0500

    Fixed the companies function to retrieve stock tickers and symbols

--- src/marketdata/financialdata/tickers.py
+++ src/marketdata/financialdata/tickers.py
@@ -16,12 +16,18 @@ import string
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 import asyncio, aiohttp, httpx
+from loguru import logger
 
+PATH = "../tmp/"
+FILE = "scraper.log"
+LOG_FILE = os.path.join(PATH, FILE)
+logger.add(LOG_FILE, format="{time:MM/DD/YYYY at HH:mm:ss} | {level} | {name} | {message}", diagnose=True, backtrace=True)
 
-
-def companies(self):
+def companies():
 
     exchanges = ["NYSE", "NASDAQ", "AMEX", "OTCBB"]
+
+    columns = ["exchange", "symbol", "name"]
 
     def get_companies(exchange="NYSE"):
         """
@@ -33,15 +39,13 @@ def companies(self):
         the market exchange passed to the function
         """
         alpha = list(string.ascii_uppercase)
-
-        symbols = []
-        name = []
-
+        symbols = list()
+        names = list()
         # loop through the letters in the alphabet to get the stocks on each page
         # from the table and store them in a list
+        logger.info("Get stock market tickers for {} exchange", exchange)
         for each in alpha:
-            url = "http://eoddata.com/stocklist/{}/{}.htm".format(
-                exchange, each)
+            url = "http://eoddata.com/stocklist/{}/{}.htm".format(exchange, each)
             resp = requests.get(url)
             site = resp.content
             soup = bs(site, 'html.parser')
@@ -49,110 +53,66 @@ def companies(self):
             for row in table.findAll('tr')[1:]:
                 symbols.append(row.findAll('td')[0].text.rstrip())
             for row in table.findAll('tr')[1:]:
-                name.append(row.findAll('td')[1].text.rstrip())
+                names.append(row.findAll('td')[1].text.rstrip())
+            logger.info("Extracted tickers from {} for {} exchange", url, exchange)
 
-        # remove the extra letters on the end of the symbols
-        symbols_clean = []
-        name_clean = []
+        clean_symbols = [symbol.replace('.','-').split('-')[0] for symbol in symbols]
+        clean_names = [n.replace('.','-').split('-')[0] for n in names]
+        logger.info("Cleaned the symbols and names by removing hyphens and additional letters")
 
-        for each in symbols:
-            each = each.replace('.', '-')
-            symbols_clean.append((each.split('-')[0]))
+        companies = list(zip(clean_symbols, clean_names))
+        logger.info("Extracted {} groups of company symbols and names:", len(companies))
 
-        for each in name:
-            each = each.replace('.', '-')
-            name_clean.append((each.split('-')[0]))
+        # turn companies list into a set:
+        companies = set(companies)
+        logger.info("{} left after removing duplicates:", len(companies))
 
-        return name_clean, symbols_clean
+        suffixes = ['Q', 'W', 'C', 'R', 'P', 'F']
 
-    NYSE_company, NYSE_symbol = get_companies(exchanges[0])
-    NASDAQ_company, NASDAQ_symbol = get_companies(exchanges[1])
-    AMEX_company, AMEX_symbol = get_companies(exchanges[2])
-    OTCBB_company, OTCBB_symbol = get_companies(exchanges[3])
+        # Create a del_set and save_set
+        del_set = set()
+        save_set = set()
 
-    columns = ["exchange", "symbol", "name"]
+        for symbol in companies:
+            if len(symbol[0]) > 4 and symbol[0][-1] in suffixes:
+                del_set.add(symbol)
+            else:
+                save_set.add(symbol)
+        logger.info("Removed {} unqualified symbols:", len(del_set))
+        logger.info("Kept {} qualified symbols:", len(save_set))
 
-    # New York Stock Exchange companies
-    NYSE = list(zip(NYSE_symbol, NYSE_company))
+        return list(save_set)
+
+    logger.info("[+] Begin extracting company information:")
+    NYSE = get_companies(exchanges[0])
+    NASDAQ = get_companies(exchanges[1])
+    AMEX = get_companies(exchanges[2])
+    OTCBB = get_companies(exchanges[3])
+
+    logger.info("[+] Add the exchange next to symbol and name")
     NYSE = [("NYSE",) + elem for elem in NYSE]
-    NYSE_df = pd.DataFrame([x for x in NYSE], columns=columns)
-
-    # NASDAQ Companies
-    NASDAQ = list(zip(NASDAQ_symbol, NASDAQ_company))
     NASDAQ = [("NASDAQ",) + elem for elem in NASDAQ]
-    NASDAQ_df = pd.DataFrame([x for x in NASDAQ], columns=columns)
-
-    # American Stock Exchange Companies
-    AMEX = list(zip(AMEX_symbol, AMEX_company))
     AMEX = [("AMEX",) + elem for elem in AMEX]
-    AMEX_df = pd.DataFrame([x for x in AMEX], columns=columns)
-
-    # Over the Counter Bulletin Board Exchange "Pink Sheets"
-    # These are the penny stocks and I think their is a lot of
-    # possibilities with finding a niche in here
-    OTCBB = list(zip(OTCBB_symbol, OTCBB_company))
     OTCBB = [("OTCBB",) + elem for elem in OTCBB]
-    OTCBB_df = pd.DataFrame([x for x in OTCBB], columns=columns)
 
-    # Now we append all the dataframes together so that we have
-    # one massive master list. Also, the good think is we can still
-    # use the smaller datasets if need be.
+    NYSE = set(NYSE)
+    NASDAQ = set(NASDAQ)
+    AMEX = set(AMEX)
+    OTCBB = set(OTCBB)
 
-    companies_df = NYSE_df.append(NASDAQ_df)
-    companies_df = companies_df.append(AMEX_df)
-    companies_df = companies_df.append(OTCBB_df)
+    logger.info("[+] Join the company sets")
+    companies = set.union(NYSE, NASDAQ, AMEX, OTCBB)
+
+    companies = list(companies)
+    logger.info("[+] Create company dataframe")
+    df = pd.DataFrame([x for x in companies], columns=columns)
 
     # Now check for duplicates and drop them from the main dataset
-    companies_df = companies_df.drop_duplicates(
+    logger.info("[+] One last check to drop duplicate symbols and names:")
+    df = df.drop_duplicates(
         subset="symbol", keep="first")
-    companies_df = companies_df.drop_duplicates(
+    df = df.drop_duplicates(
         subset="name", keep="first")
 
-    return companies_df
-
-
-# =========================================================================== #
-# Fix this function to be neater and handle the cleaning processes:
-# =========================================================================== #
-
-def get_companies(exchange="NYSE"):
-    """
-    :param exchange: The Stock exchange for which you want
-    to get a current list of all the symbols for.
-    Default -> NYSE
-
-    :returns: a list of tuples containing every company name and symbol in
-    the market exchange passed to the function
-    """
-    alpha = list(string.ascii_uppercase)
-
-    symbols = []
-    name = []
-
-    # loop through the letters in the alphabet to get the stocks on each page
-    # from the table and store them in a list
-    for each in alpha:
-        url = "http://eoddata.com/stocklist/{}/{}.htm".format(
-            exchange, each)
-        resp = requests.get(url)
-        site = resp.content
-        soup = bs(site, 'html.parser')
-        table = soup.find('table', {'class': 'quotes'})
-        for row in table.findAll('tr')[1:]:
-            symbols.append(row.findAll('td')[0].text.rstrip())
-        for row in table.findAll('tr')[1:]:
-            name.append(row.findAll('td')[1].text.rstrip())
-
-    # remove the extra letters on the end of the symbols
-    symbols_clean = []
-    name_clean = []
-
-    for each in symbols:
-        each = each.replace('.', '-')
-        symbols_clean.append((each.split('-')[0]))
-
-    for each in name:
-        each = each.replace('.', '-')
-        name_clean.append((each.split('-')[0]))
-
-    return name_clean, symbols_clean
+    logger.success("Retrieved all the company stock symbols and names. Returning a pandas dataframe:")
+    return df
